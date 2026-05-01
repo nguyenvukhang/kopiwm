103f5af900316550c2d371b562ba8a8af0a591c9
nguyenvukhang <brew4k@gmail.com>
2026-05-01 17:25:57 +0800

    Ported xfontCreate

--- zig/src/drw.zig
+++ zig/src/drw.zig
@@ -1,5 +1,7 @@
+const std = @import("std");
 const x = @import("c_lib.zig").x;
 const fc = @import("c_lib.zig").fc;
+const mem = std.mem;
 
 pub const Cursor = x.Cursor;
 pub const Display = x.Display;
@@ -12,7 +14,7 @@ pub const Fnt = struct {
     dpy: ?*Display,
     h: u16,
     xfont: ?*x.XftFont,
-    fc: ?*fc.FcPattern,
+    pattern: ?*fc.FcPattern,
     next: ?*Fnt,
 };
 
@@ -34,6 +36,52 @@ pub const ColorScheme = struct {
     border: XftColor,
 };
 
+/// [dwm] xfont_create
+fn xfontCreate(
+    allocator: mem.Allocator,
+    drw: *Drw,
+    fontname: []const u8,
+    font_pattern: ?*x.FcPattern,
+) !?*Fnt {
+    var xfont: ?*x.XftFont = null;
+    var pattern: ?*x.FcPattern = null;
+
+    if (fontname.len > 0) {
+        // Using the pattern found at font->xfont->pattern does not yield the
+        // same substitution results as using the pattern returned by
+        // FcNameParse; using the latter results in the desired fallback
+        // behaviour whereas the former just results in missing-character
+        // rectangles being drawn, at least with some fonts.
+        xfont = x.XftFontOpenName(drw.dpy, drw.screen, fontname);
+        if (xfont == null) {
+            std.debug.print("error, cannot load font from name: '{s}'\n", .{fontname});
+            return null;
+        }
+        pattern = x.FcNameParse(fontname);
+        if (pattern == null) {
+            std.debug.print("error, cannot parse font name to pattern: '{s}'\n", .{fontname});
+            x.XftFontClose(drw.dpy, xfont);
+            return null;
+        }
+    } else if (font_pattern) |fp| {
+        xfont = x.XftFontOpenPattern(drw.dpy, fp);
+        if (xfont == null) {
+            std.debug.print("error, cannot load font from pattern\n", .{});
+            return null;
+        }
+    } else {
+        @panic("No font specified.");
+    }
+
+    var font = try allocator.create(Fnt);
+    font.xfont = xfont;
+    font.pattern = pattern;
+    font.h = xfont.?.ascent + xfont.?.descent;
+    font.dpy = drw.dpy;
+
+    return font;
+}
+
 pub const Drw = struct {
     const Self = @This();
 
@@ -50,7 +98,7 @@ pub const Drw = struct {
     /// A linked list of fonts.
     fonts: ?*Fnt = null,
 
-    /// Replaces `drw_create` in dwm.
+    /// [dwm] drw_create
     pub fn init(
         dpy: *Display,
         screen: c_int,
@@ -73,6 +121,7 @@ pub const Drw = struct {
         return drw;
     }
 
+    /// [dwm] drw_resize
     pub fn resize(self: *Self, w: u32, h: u32) void {
         self.w = w;
         self.h = h;
@@ -88,6 +137,7 @@ pub const Drw = struct {
         );
     }
 
+    /// [dwm] drw_free
     pub fn deinit(self: *Self) void {
         x.XFreePixmap(self.dpy, self.drawable);
         x.XFreeGC(self.dpy, self.gc);
@@ -95,5 +145,28 @@ pub const Drw = struct {
         // TODO:  port this line of C:
         // drw_fontset_free(drw->fonts);
 
+    }
+
+    /// [dwm] drw_fontset_create
+    pub fn fontset_create(self: *Self, fonts: [][]const u8) ?*Fnt {
+        if (fonts.len == 0) {
+            return null;
+        }
+        for (fonts) |font| {
+            xfontCreate(self, font, null);
+        }
+
+        return self.fonts;
+
+        // Fnt *cur, *ret = NULL;
+        // size_t i;
+        //
+        // for (i = 1; i <= fontcount; i++) {
+        //     if ((cur = xfont_create(drw, fonts[fontcount - i], NULL))) {
+        //         cur->next = ret;
+        //         ret = cur;
+        //     }
+        // }
+        // return (drw->fonts = ret);
     }
 };
